3b954c128a10a1e303fed09ffeffa270d59583c8
Davido Alsadiva <147804398+Davidoasdv@users.noreply.github.com>
2026-09-03 17:19:25 +0700

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -10,8 +10,8 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@400;500;600;700&family=Plus+Jakarta+Sans:wght@400;500;600;700&family=Bricolage+Grotesque:wght@600;700&display=swap" rel="stylesheet">
     <!-- CSS -->
-    <link rel="stylesheet" href="assets/css/style.css">
-    <link rel="stylesheet" href="assets/css/components.css">
-    <link rel="stylesheet" href="assets/css/responsive.css">
+    <link rel="stylesheet" href="assets/css/style.css?v=3">
+    <link rel="stylesheet" href="assets/css/components.css?v=3">
+    <link rel="stylesheet" href="assets/css/responsive.css?v=3">
 </head>
 <body>
