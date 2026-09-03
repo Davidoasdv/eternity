195b8429b18945d486d2983a41a2eeae4555e572
Davido Alsadiva <147804398+Davidoasdv@users.noreply.github.com>
2026-09-03 17:03:24 +0700

    Update navbar.php

--- includes/navbar.php
+++ includes/navbar.php
@@ -5,13 +5,14 @@
             <span></span><span></span><span></span>
         </button>
         <ul class="nav-menu" id="navMenu">
-            <li><a href="/eternity/index.php">Home</a></li>
-            <li><a href="/eternity/story.php">Our Story</a></li>
-            <li><a href="/eternity/letters.php">Letters</a></li>
-            <li><a href="/eternity/wishlist.php">Wishlist</a></li>
-            <li><a href="/eternity/songs.php">Our Songs</a></li>
-            <li><a href="/eternity/future.php">Future</a></li>
-            <li><a href="/eternity/settings.php">Settings</a></li>
+            <a href="/eternity/" class="nav-brand">ETERNITY</a>
+<li><a href="/eternity/index.php">Home</a></li>
+<li><a href="/eternity/story.php">Our Story</a></li>
+<li><a href="/eternity/letters.php">Letters</a></li>
+<li><a href="/eternity/wishlist.php">Wishlist</a></li>
+<li><a href="/eternity/songs.php">Our Songs</a></li>
+<li><a href="/eternity/future.php">Future</a></li>
+<li><a href="/eternity/settings.php">Settings</a></li>
         </ul>
     </div>
 </nav>
