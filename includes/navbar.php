62d2ab5349529e640bf2448a1c74998e8d70b2a9
Davido Alsadiva <147804398+Davidoasdv@users.noreply.github.com>
2026-09-03 17:15:27 +0700

    Update navbar.php

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,19 +1,17 @@
 <nav class="navbar">
     <div class="nav-container">
-        <a href="/eternity/" class="nav-brand">ETERNITY</a>
+        <a href="index.php" class="nav-brand">ETERNITY</a>
         <button class="nav-toggle" id="navToggle" aria-label="Toggle navigation">
             <span></span><span></span><span></span>
         </button>
         <ul class="nav-menu" id="navMenu">
-            <a href="/eternity/" class="nav-brand">ETERNITY</a>
-<a href="index.php" class="nav-brand">ETERNITY</a>
-<li><a href="index.php">Home</a></li>
-<li><a href="story.php">Our Story</a></li>
-<li><a href="letters.php">Letters</a></li>
-<li><a href="wishlist.php">Wishlist</a></li>
-<li><a href="songs.php">Our Songs</a></li>
-<li><a href="future.php">Future</a></li>
-<li><a href="settings.php">Settings</a></li>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="story.php">Our Story</a></li>
+            <li><a href="letters.php">Letters</a></li>
+            <li><a href="wishlist.php">Wishlist</a></li>
+            <li><a href="songs.php">Our Songs</a></li>
+            <li><a href="future.php">Future</a></li>
+            <li><a href="settings.php">Settings</a></li>
         </ul>
     </div>
 </nav>
